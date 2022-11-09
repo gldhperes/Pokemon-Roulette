e15debd59ff44ac7c380e5d9c537c823f94d5213
unknown <gldhperes@gmail.com>
2022-11-09 13:39:40 -0300

    Updates

---
+++ precache-manifest.fed281cefb5563eff6c106a903341f78.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5de3489563bd7911141",
+    "url": "/Pokemon-Roulette/static/css/main.12d49835.chunk.css"
+  },
+  {
+    "revision": "d5de3489563bd7911141",
+    "url": "/Pokemon-Roulette/static/js/main.f5e3d753.chunk.js"
+  },
+  {
+    "revision": "32012ea83a1994d78877",
+    "url": "/Pokemon-Roulette/static/js/runtime~main.67b9adf2.js"
+  },
+  {
+    "revision": "91c8ff4e51a2f87f89df",
+    "url": "/Pokemon-Roulette/static/js/2.4f8bf354.chunk.js"
+  },
+  {
+    "revision": "9ba4d684e64c19af7c99148ac36af46b",
+    "url": "/Pokemon-Roulette/static/media/bulbasaur.9ba4d684.png"
+  },
+  {
+    "revision": "a265bdc3176f3fbb9acae21c5a800b89",
+    "url": "/Pokemon-Roulette/static/media/charmander.a265bdc3.png"
+  },
+  {
+    "revision": "f0be8e609d5b0fa86359426470ab689d",
+    "url": "/Pokemon-Roulette/static/media/squirtle.f0be8e60.png"
+  },
+  {
+    "revision": "53dbfd304766b5858ee6e139646713c7",
+    "url": "/Pokemon-Roulette/static/media/pikachu.53dbfd30.png"
+  },
+  {
+    "revision": "73819dcb43293deef52004751ce0dd5a",
+    "url": "/Pokemon-Roulette/static/media/chikorita.73819dcb.png"
+  },
+  {
+    "revision": "6970370201b70f06ca7e9749e5343492",
+    "url": "/Pokemon-Roulette/static/media/cyndaquil.69703702.png"
+  },
+  {
+    "revision": "1b04c3e4baf2805e335b275603860bcc",
+    "url": "/Pokemon-Roulette/static/media/totodile.1b04c3e4.png"
+  },
+  {
+    "revision": "ec41b7845dd064f59c708f21ab4c7366",
+    "url": "/Pokemon-Roulette/static/media/unown.ec41b784.png"
+  },
+  {
+    "revision": "ebf8879740a858e9438944ccb7405eed",
+    "url": "/Pokemon-Roulette/static/media/treecko.ebf88797.png"
+  },
+  {
+    "revision": "526e7a4ca5e91ae7894c532c3aaad160",
+    "url": "/Pokemon-Roulette/static/media/torchic.526e7a4c.png"
+  },
+  {
+    "revision": "15499e0a9d2b933f0be0ce505479a286",
+    "url": "/Pokemon-Roulette/static/media/piplup.15499e0a.png"
+  },
+  {
+    "revision": "aeabfbec2d934453638172af6e23d0b0",
+    "url": "/Pokemon-Roulette/static/media/aron.aeabfbec.png"
+  },
+  {
+    "revision": "66d8c6f2d05bd515b67be795e5eba13e",
+    "url": "/Pokemon-Roulette/static/media/bagon.66d8c6f2.png"
+  },
+  {
+    "revision": "7475b27946f75051dfa7d8d6dc5e9a27",
+    "url": "/Pokemon-Roulette/index.html"
+  }
+];
